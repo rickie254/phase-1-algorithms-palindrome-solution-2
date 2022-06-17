eb0ba5fd4f6dcaf4e2416892689a140aa07f8859
rickie maweu <rickiemaweu@gmail.com>
2022-06-17 22:42:14 +0300

    commit tree fcb3b0cec4519ec851e889f4758540f41b0954d6

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  // iterate from the beginning of the string to the middle of the string
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    // compare the letter we're iterating over to the corresponding letter at the end of the string
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      // if the letters don't match, return false
+      return false;
+    }
+  }
 
+  // if we reach the middle, and all the letters match, return true
+  return true;
+}
 /* 
   Add your pseudocode here
+  iterate from the beginning of the string to the middle of the string
+  compare the letter we're iterating over to the corresponding letter at the end of the string
+    if the letters don't match, return false
+  if we reach the middle, and all the letters match, return true
 */
 
 /*
   Add written explanation of your solution here
+  I need to make an isPalindrome function that returns either true or false. When the input string is the same forwards and backwards, I should return true. That means if the first letter is the same as the last letter, and the second letter is the same as the second to last letter, and so on (until the middle of the word), the function should return true. If the input string is not the same forwards and backwards, I should return false.
 */
 
 // You can run `node index.js` to view these console logs
